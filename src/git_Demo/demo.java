a9b2abd15a06e9629e56e937e17cbcd3386057e1
srinidhitekarch <srinidhi.b@tekarch.com>
2020-04-13 23:46:07 +0530

    first commit of my Demo mar2020

---
+++ src/git_Demo/demo.java
@@ -0,0 +1,36 @@
+package git_Demo;
+
+public class demo {
+
+	public static void main(String[] args) {
+		/*
+		 i have video file --> i want to share it to ABhi --> pendrive -- Not better option
+		 Upload it into cloud.. Googles drive, Third party cloud space, I say i will give link:
+		 See the content of it.
+		 part_1 + Part_2
+		 .ppt 10 slides + if you start creating another 2 shared to him 
+		 * */
+
+	//GIT :  version control system for tracking changes in source code in development activity
+	// Repo : place where you can store your code
+	
+		System.out.println("Im learning git today");
+		
+		//On Branch MASTER
+		
+		// GIT HUB --> GIT tool , 
+		
+		// Our code to cloud.. Moving my git_Demo project to cloud
+		//if i have to move one project --> Separate repository under git hub to maintain all projects in different folder  
+		// how do you we create repository --> learnt
+		// How do you push your code to Repository
+		// 1. Git set up to project is not done --> First we have to intialise the git to the project
+		// Git is intialised and it is ready to transfer the code to cloud
+		// 3 steps are there (Copy your code to local git repo and then same thing push it Cloud Repo)
+		// Git add --> It will save the code
+		// Git commit --> It will commit to the local git Repo
+		// Git Push  --> It will commit/Pushed to the cloud Repo
+	}
+	
+
+}
